2e35a1fd075cad2d7ddb2491305ddfbc10c8beca
Pablo Henrique <pablohenrique.r.c@gmail.com>
2022-12-15 19:40:20 -0300

    Implementa importação de clientes diretamente para um grupo

--- src/screens/Member/ImportClients.tsx
+++ src/screens/Member/ImportClients.tsx
@@ -1,10 +1,11 @@
-import { VStack, Text, Button, Input } from "native-base";
-import { useState } from "react";
+import { VStack, Text, Button, Input, Select, CheckIcon } from "native-base";
+import { useEffect, useState } from "react";
 
 import { useDrawer } from "../../contexts/drawer";
 
 import { useNavigation } from "@react-navigation/native";
 import api from "../../services/api";
+import { GroupProps } from "../../components/Group";
 
 export const ImportClients = () => {
   const navigation = useNavigation();
@@ -13,23 +14,42 @@ export const ImportClients = () => {
 
   const [data, setData] = useState("");
 
+  const [groups, setGroups] = useState<GroupProps[]>([]);
+  const [selectedGroupId, setSelectedGroupId] = useState<string>();
+
+  useEffect(() => {
+    api
+      .get(`v1/groups/organizations/${organizationId}`)
+      .then(({ data }) => {
+        console.log(data);
+        setGroups(data);
+        setSelectedGroupId(data[0].id);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   const handleImportClients = () => {
     const line = data.split("\n");
     let clients = [];
 
     for (let i = 0; i < line.length; i++) {
-      clients.push(line[i].split("\t"));
+      const client = line[i].split("\t");
+
+      clients.push({
+        name: client[1],
+        registrationId: client[0],
+        organizationId,
+      });
     }
 
-    Promise.all(
-      clients.map((client) =>
-        api.post("v1/clients", {
-          name: client[1],
-          registrationId: client[0],
-          organizationId,
-        })
-      )
-    )
+    api
+      .post("v1/groups/import", {
+        clients,
+        organizationId,
+        groupId: selectedGroupId,
+      })
       .then(() => {
         navigation.goBack();
       })
@@ -47,6 +67,22 @@ export const ImportClients = () => {
         w="100%"
         space={4}
       >
+        <Select
+          selectedValue={selectedGroupId}
+          accessibilityLabel="Escolha um grupo"
+          placeholder="Escolha um grupo"
+          _selectedItem={{
+            bg: "teal.600",
+            endIcon: <CheckIcon size="5" />,
+          }}
+          mt={1}
+          onValueChange={(id) => setSelectedGroupId(id)}
+        >
+          {groups.map((group) => (
+            <Select.Item label={group.name} value={group.id} />
+          ))}
+        </Select>
+
         <Input
           multiline
           numberOfLines={5}
