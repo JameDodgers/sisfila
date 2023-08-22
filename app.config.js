a4d0a04194d541e4ed9546c60f4b83d219ec2f97
Pablo Henrique <pablohenrique.r.c@gmail.com>
2023-08-21 22:53:50 -0300

    Usa webpack

--- app.config.js
+++ app.config.js
@@ -29,7 +29,6 @@ module.exports = {
     },
     web: {
       favicon: "./assets/favicon.png",
-      bundler: "metro",
     },
     extra: {
       expoClientId: process.env.EXPO_PUBLIC_EXPO_CLIENT_ID,
