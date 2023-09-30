5add9bf8cea7a3dabc3835eae5d2a45b0786dd04
Pablo Henrique <pablohenrique.r.c@gmail.com>
2023-09-30 18:59:05 -0300

    Usa forwardRef no CustomTextInput

--- src/components/CustomTextInput.tsx
+++ src/components/CustomTextInput.tsx
@@ -1,12 +1,19 @@
+import { forwardRef } from "react";
+import { TextInput } from "react-native";
 import { Platform } from "react-native";
 import {
   TextInput as PaperTextInput,
   TextInputProps,
 } from "react-native-paper";
 
-export const CustomTextInput = (props: TextInputProps) => {
+type Props = TextInputProps;
+
+export type Ref = TextInput;
+
+export const CustomTextInput = forwardRef<Ref, Props>((props, ref) => {
   return (
     <PaperTextInput
+      ref={ref}
       className="web:bg-transparent"
       mode={Platform.select({
         native: "outlined",
@@ -15,4 +22,4 @@ export const CustomTextInput = (props: TextInputProps) => {
       {...props}
     />
   );
-};
+});
